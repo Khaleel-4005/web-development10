1b7bc50937b20edf710acdf385545e47213bcf86
Khaleel-4005 <khaleel4005@gmail.com>
2025-01-13 21:49:42 +0530

    update.code

--- LOG/log.Script.js
+++ LOG/log.Script.js
@@ -10,3 +10,35 @@ document.addEventListener("DOMContentLoaded", function () {
         alert("Google login functionality is not implemented yet.");
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById('add-btn').addEventListener('click', addTodo);
+
+function addTodo() {
+    const input = document.getElementById('todo-input');
+    const task = input.value.trim();
+
+    if (task !== "") {
+        const list = document.getElementById('todo-list');
+        const li = document.createElement('li');
+        li.innerHTML = `${task} <span onclick="removeTask(this)">❌</span>`;
+        list.appendChild(li);
+        input.value = "";
+    } else {
+        alert('Please enter a task!');
+    }
+}
+
+function removeTask(span) {
+    const li = span.parentElement;
+    li.remove();
+}
