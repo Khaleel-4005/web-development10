ce0e62256977d69416a7360f4ac49045f4e9be11
Khaleel-4005 <khaleel4005@gmail.com>
2025-01-14 21:28:01 +0530

    update

--- LOG/log.Script.js
+++ LOG/log.Script.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const signInBtn = document.querySelector(".sign-in-btn");
-    signInBtn.addEventListener("click", function (event) {
-        event.preventDefault();
-        alert("Sign in functionality is not implemented yet.");
-    });
+// document.addEventListener("DOMContentLoaded", function () {
+//     const signInBtn = document.querySelector(".sign-in-btn");
+//     signInBtn.addEventListener("click", function (event) {
+//         event.preventDefault();
+//         alert("Sign in functionality is not implemented yet.");
+//     });
 
-    const googleBtn = document.querySelector(".google-btn");
-    googleBtn.addEventListener("click", function () {
-        alert("Google login functionality is not implemented yet.");
-    });
-});
+//     const googleBtn = document.querySelector(".google-btn");
+//     googleBtn.addEventListener("click", function () {
+//         alert("Google login functionality is not implemented yet.");
+//     });
+// });
 
 
 
